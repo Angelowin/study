576a581c28006de864aeaf278b99eef2b3aaba95
Angelowin <917306687@qq.com>
2018-09-05 09:48:00 +0800

    add algorithm_1

---
+++ leetcode/algorithm_1.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        nums = nums
+        target = target
+
+        for num1, num2 in enumerate(nums):
+            if target - num2 in nums[num1 + 1:]:
+                return num1, nums[num1 + 1:].index(target - num2) + num1 + 1
--- niuke/study.py
+++ niuke/study.py
@@ -1,1 +1,26 @@
-import numpy
+class Employee():
+    raiseAmount = 1.05
+
+    def __init__(self,first,surname,salary):
+        self.first = first
+        self.surname = surname
+        self.salary = salary
+        self.email = first + "" + surname + "@PythonABC.com"
+
+    def infoSummary(self):
+        return "{}, {}, {}".format(self.first + " " + self.surname, self.salary,self.email)
+
+    def raiseSalary(self):
+        self.salary = self.salary * Employee.raiseAmount
+
+employee1 = Employee("Harry", "Potter",4000)
+employee2 = Employee("Bilbo", "Baggins",6000)
+employee3 = Employee("Mark", "Twain",8000)
+employee4 = Employee("Julius", "Caesar",12000)
+
+print(employee1.infoSummary())
+print(employee2.infoSummary())
+
+employee1.raiseSalary()
+print(employee1.infoSummary())
+print(employee2.infoSummary())
